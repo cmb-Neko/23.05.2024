05b87b93f48241b85e9250ec95a0540a65b3a9ba
cmb-Neko <h1t0nek0uwu@gmal.com>
2024-05-23 20:50:59 +0300

    hm_1_unfinished

--- main.cpp
+++ main.cpp
@@ -9,48 +9,16 @@ int main() {
 	int arch_arr[memo_supreme];
 	int arr_1[memo_lesser];
 	int	arr_2[memo_lesser];
-	for (int i = 0; i < memo_lesser; i++) {
-		arr_1[i] = rand() % 20 - 10;
-		arr_2[i] = rand() % 20 - 10;
+	for (int i = 0; i < memo_supreme; i++) {
+		arch_arr[i] = rand() % 2 + 0;
 	}
-
-	for (int i = 0; i < memo_lesser; i++) {
-		cout << arr_1[i] << " ";
-	}
-	cout << endl;
-	for (int i = 0; i < memo_lesser; i++) {
-		cout << arr_2[i] << " ";
-	}
-
-	cout << endl;
 
 	int j = 0;
-	for (int i = 0; i < memo_lesser; i++) {
-		if (arr_1[i] > 0) {
-			arch_arr[j++] = arr_1[i];
-		}
-		if (arr_2[i] > 0) {
-			arch_arr[j++] = arr_2[i];
-		}
+	for (int i = 0; i < memo_supreme; i += 2) {
+		arr_1[j] = arch_arr[i - 1];
+		arr_2[j] = arch_arr[i];
 	}
 	for (int i = 0; i < memo_lesser; i++) {
-		if (arr_1[i] == 0) {
-			arch_arr[j++] = arr_1[i];
-		}
-		if (arr_2[i] == 0) {
-			arch_arr[j++] = arr_2[i];
-		}
-	}
-	for (int i = 0; i < memo_lesser; i++) {
-		if (arr_1[i] < 0) {
-			arch_arr[j++] = arr_1[i];
-		}
-		if (arr_2[i] < 0) {
-			arch_arr[j++] = arr_2[i];
-		}
-	}
-
-	for (int i = 0; i < memo_supreme; i++) {
-		cout << arch_arr[i] << " ";
+		cout << arr_
 	}
 }
